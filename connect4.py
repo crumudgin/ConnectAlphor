3018ae84371efd2f96548c245e3888255d2df78f
zac <zac.chu@gmail.com>
2018-02-28 08:05:28 -0500

    implemented winChecking

--- connect4.py
+++ connect4.py
@@ -2,8 +2,8 @@ from game import Game
 import numpy as np
 
 class connect4(Game):
-	self.X = 6
-	self.Y = 7
+	X = 6
+	Y = 7
 
 	"""
 	Name: generateBoard
@@ -27,26 +27,57 @@ class connect4(Game):
 		for col in range(0, len(self.board)):
 			for row in range(0, len(self.board[col])):
 				if self.board[col,row] != 0:
-					h = checkHorizontal(col, row)
-					v = checkVertical(col, row)
-					tl = checkDiagnalTopLeftBottomRight(col, row)
-					tr = checkDiagnalTopRightBottomLeft(col, row)
+					h = self.checkHorizontal(col, row)
+					v = self.checkVertical(col, row)
+					tl = self.checkDiagnalTopLeftBottomRight(col, row)
+					tr = self.checkDiagnalTopRightBottomLeft(col, row)
 					if h or v or tl or tr:
 						return self.board[col,row]
 		return 0
 
 
-	def checkHorizontal(col, row, winCondition=4):
-		pass
+	def checkHorizontal(self, col, row, winCondition=4):
+		if row + winCondition < len(self.board):
+			# print(rwinCondition+1)
+			for i in range(1, winCondition+1):
+				# print(i)
+				# print(self.board[col, row+i], self.board[col, row])
+				if self.board[col,row+i] != self.board[col,row]:
+					# print(self.board[col, row+i], self.board[col, row])
+					return False
+			return True
+		return False
 
-	def checkVertical(col, row, winCondition=4):
-		pass
+	def checkVertical(self, col, row, winCondition=4):
+		# print(col+winCondition)
+		# print(len(self.board[col]))
+		# print((row + winCondition) < len(self.board[col]))
+		if col + winCondition < len(self.board[col]):
+			for i in range(1, winCondition+1):
+				print(i)
+				print(self.board[col+i, row], self.board[col, row])
+				if self.board[col+i,row] != self.board[col,row]:
+					print(self.board[col+i, row], self.board[col, row])
+					return False
+			return True
+		return False
 
-	def checkDiagnalTopLeftBottomRight(col, row, winCondition=4):
-		pass
+	def checkDiagnalTopLeftBottomRight(self, col, row, winCondition=4):
+		if row + winCondition < len(self.board[col]) and col + winCondition < len(self.board):
+			for i in range(1, winCondition+1):
+				if self.board[col+i,row+i] != self.board[col,row]:
+					return False
+			return True
+		return False
 
-	def checkDiagnalTopRightBottomLeft(col, row, winCondition=4):
-		pass
+	def checkDiagnalTopRightBottomLeft(self, col, row, winCondition=4):
+		if row + winCondition < len(self.board[col]) and col + winCondition >= 0:
+			for i in range(1, winCondition+1):
+				if self.board[col-i,row+i] != self.board[col,row]:
+					return False
+			return True
+		return False
+
 
 	def move(self, player, move):
 		col = self.board[:,move]
@@ -60,3 +91,13 @@ class connect4(Game):
 		newGame = connect4(self.x, self.y, self.board)
 		newGame.move(player, move)
 		return newGame
+
+c = connect4(6,7)
+c.move(1, 3)
+c.move(-1, 3)
+c.move(-1, 3)
+c.move(-1, 3)
+c.move(-1, 3)
+c.move(-1, 3)
+print(c.getBoard())
+print(c.gameEnd())
