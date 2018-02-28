c8c99dccc08b30fcc7eec03a96a47aecc7243494
zac <zac.chu@gmail.com>
2018-02-27 23:39:46 -0500

    implemented move and legalMoves methods

--- connect4.py
+++ connect4.py
@@ -8,13 +8,18 @@ class connect4(Game):
 			return np.zeros((self.x, self.y))
 
 	def legalMoves(self):
-		pass
+		return [i for i in range(0, len(self.board[0])) if self.board[0,i] == 0]
 
 	def gameEnd(self):
 		pass
 
 	def move(self, player, move):
-		pass
+		col = self.board[:,move]
+		lastZero = 0
+		for index, i in enumerate(col):
+			if i == 0:
+				lastZero = index
+		self.board[lastZero,move] = player
 
 	def copyAndMove(self, player, move):
 		pass
